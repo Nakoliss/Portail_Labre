10f391413e1661294c1d7ff6d0b4051287c857da
Nakoliss <nakoliss@nadiel.com>
2023-05-30 09:32:30 -0400

    Push everything to be use on server

--- loadNavbar.js
+++ loadNavbar.js
@@ -1,3 +1,21 @@
 $(function () {
-    $("#navbar").load("navbar.html");
+    $("#navbar").load("navbar.html", function () {
+        const loggedIn = "N"; //
+
+        if (loggedIn === "N") {
+            showLoginWindow();
+        }
+    });
+
+    $("#closeLoginWindow").click(function () {
+        hideLoginWindow();
+    });
 });
+
+function showLoginWindow() {
+    $("#loginWindowContainer").show();
+}
+
+function hideLoginWindow() {
+    $("#loginWindowContainer").hide();
+}
